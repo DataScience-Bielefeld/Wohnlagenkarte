07e8a2b0a4131ced653257bda763956c5375415d
gllrt <35564383+gllrt@users.noreply.github.com>
2018-11-24 12:10:58 +0100

    Add tiles from overpass API to map

--- Code Snippets/Shiny_App/server.R
+++ Code Snippets/Shiny_App/server.R
@@ -3,26 +3,21 @@ library(RColorBrewer)
 library(scales)
 library(lattice)
 library(dplyr)
-library(overpass)
+library(overpass) # See https://github.com/hrbrmstr/overpass
 
 function(input, output, session) {
 
+  ## Interactive Map ###########################################
+
   # Create the map
   output$leaflet_map <- renderLeaflet({
-    leaflet() %>%
-      addTiles(
-        urlTemplate = "//{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", # For more options see https://leaflet-extras.github.io/leaflet-providers/preview/
-        attribution = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, Tiles courtesy of <a href="http://hot.openstreetmap.org/" target="_blank">Humanitarian OpenStreetMap Team</a>',
-        options = providerTileOptions(opacity = 0.8)
-      ) %>%
-      setView(lng = 8.531990, lat = 52.020615, zoom = 16)
+    leaflet_object$map
   })
 
   react_coord <- reactiveValues(north = 0, east = 0, south = 0, west = 0, lat = 0, lon = 0, values = 0)
 
   map_bound_coord <- observe({
     if(!is.null(input$leaflet_map_bounds)) {
-      print(c(input$leaflet_map_bounds$`north`, input$leaflet_map_bounds$`east`, input$leaflet_map_bounds$`south`, input$leaflet_map_bounds$`west`))
       react_coord$north <- input$leaflet_map_bounds$`north`
       react_coord$east <- input$leaflet_map_bounds$`east`
       react_coord$south <- input$leaflet_map_bounds$`south`
@@ -30,11 +25,19 @@ function(input, output, session) {
       react_coord$lat <- c(react_coord$north, react_coord$north, react_coord$south, react_coord$south)
       react_coord$lon <- c(react_coord$west, react_coord$east, react_coord$east, react_coord$west)
       react_coord$values <- cbind(react_coord$lat, react_coord$lon)
-      print(input$leaflet_map_zoom)
     }
   })
 
-  counter_button <- reactiveValues(prev = 0)
+  counter_button <- reactiveValues(prev = -1)
+  leaflet_object <- reactiveValues(
+    map = leaflet() %>%
+      addTiles(
+        urlTemplate = "//{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", # For more options see https://leaflet-extras.github.io/leaflet-providers/preview/
+        attribution = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, Tiles courtesy of <a href="http://hot.openstreetmap.org/" target="_blank">Humanitarian OpenStreetMap Team</a>',
+        options = providerTileOptions(opacity = 0.8)
+      ) %>%
+      setView(lng = 8.531990, lat = 52.020615, zoom = 16)
+  )
 
   observeEvent(
     input$button_rating,
@@ -46,6 +49,39 @@ function(input, output, session) {
           closeButton = TRUE,
           type = "error"
         )
+      } else {
+        query_txt <- paste0(
+          '[out:xml][timeout:25];
+          (
+          node["building"](', react_coord$south, ',', react_coord$west, ',', react_coord$north, ',', react_coord$east, ');
+          way["building"](', react_coord$south, ',', react_coord$west, ',', react_coord$north, ',', react_coord$east, ');
+          relation["building"](', react_coord$south, ',', react_coord$west, ',', react_coord$north, ',', react_coord$east, ');
+          );
+          out body;
+          >;
+          out skel qt;'
+        )
+
+        building_polygons <- overpass::overpass_query(query_txt)
+
+        lf <- leaflet(data = building_polygons) %>%
+          addTiles(
+            urlTemplate = "//{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", # For more options see https://leaflet-extras.github.io/leaflet-providers/preview/
+            attribution = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, Tiles courtesy of <a href="http://hot.openstreetmap.org/" target="_blank">Humanitarian OpenStreetMap Team</a>',
+            options = providerTileOptions(opacity = 0.8)
+          ) %>%
+          setView(
+            lng = input$leaflet_map_center$`lng`,
+            lat = input$leaflet_map_center$`lat`,
+            zoom = input$leaflet_map_zoom
+          )
+
+        for (i in 1:length(building_polygons@lines)) {
+          lf <- lf %>% addPolylines(data=building_polygons@lines[[i]], color = "black", weight = 2, opacity = 1.0,
+                                    fill = TRUE, fillColor = "white", fillOpacity = 1.0)
+        }
+        leaflet_object$map <- lf
+
       }
     }
   )
