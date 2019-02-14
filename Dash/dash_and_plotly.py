f78a1b5c0ec1ab2b855f5a3f479860b5807d0216
Nils Hachmeister <nils.hachmeister@mailbox.org>
2019-02-14 21:23:04 +0100

    Documented where we left the project.

--- Dash/dash_and_plotly.py
+++ Dash/dash_and_plotly.py
@@ -11,6 +11,9 @@ ice_creams = myApi.query("""node["addr:city"="Bielefeld"]["amenity"="ice_cream"]
 
 print(len(ice_creams.nodes))
 
+# The solution on the web, which we used here uses mapbox. But mapbox requires a token
+# for which we would need to pay a fee. We are currently looking for a mapbox-free solution
+# but have not found any solution which uses plotly and does not use mapbox.
 layout = graph_objs.Layout(
     height=600,
     autosize = True,
@@ -19,6 +22,8 @@ layout = graph_objs.Layout(
 
 app = dash.Dash(__name__)
 
+# Once we got that actual plot/layout working, we would need to include this into our app. The code
+# as it is here, is still from the folium-based solution.
 app.layout = html.Div([
     html.H1("Bielefeld City Map"),
     html.Iframe(id = "map", srcDoc=open("NYC_map.html", "r").read(), width="100%", height="600")
