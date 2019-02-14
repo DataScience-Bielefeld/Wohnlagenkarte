d9e1dacdead8e4262436dd23c18c4fb35cb0d0e3
Fritz Feger <ff@reg.io>
2019-02-14 20:46:38 +0100

    removed commented out stuff

--- dash/dash_and_plotly.py
+++ dash/dash_and_plotly.py
@@ -1,10 +1,7 @@
-import folium
 import plotly.plotly as plotly
 import plotly.graph_objs as graph_objs
 import dash
-import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
 import overpy
 
 BI_COORDINATES =(52.022915, 8.528429)
@@ -13,10 +10,6 @@ myApi = overpy.Overpass()
 ice_creams = myApi.query("""node["addr:city"="Bielefeld"]["amenity"="ice_cream"];out body;""")
 
 print(len(ice_creams.nodes))
-
-#f_markers = [folium.Marker([ice.lat, ice.lon], popup='Eisdiele', tooltip=r"""<p>{name}<br/>{strasse} {nr}<br/>{stadt}</p>"""
-#                           .format(name=ice.tags["name"],strasse=ice.tags["addr:street"],
-#                                   nr=ice.tags["addr:housenumber"],stadt=ice.tags["addr:city"])).add_to(my_map) for ice in ice_creams.nodes]
 
 layout = graph_objs.Layout(
     height=600,
