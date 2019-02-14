231586be102288b48a83ac669d0eb34f76267d83
Nils Hachmeister <nils.hachmeister@mailbox.org>
2019-02-14 21:18:37 +0100

    Added folium solution.

---
+++ Dash/dash_and_folium.py
@@ -0,0 +1,35 @@
+import folium
+import pandas as pd
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import overpy
+
+NY_COORDINATES =(52.022915, 8.528429)
+
+my_map = folium.Map(location=NY_COORDINATES, tiles= "Stamen Toner", zoom_start=12)
+
+# BB Bielefeld: node(51.58094, 8.25457, 52.03187, 8.37570);
+myApi = overpy.Overpass()
+ice_creams = myApi.query("""node["addr:city"="Bielefeld"]["amenity"="ice_cream"];out body;""")
+
+print(len(ice_creams.nodes))
+
+#tags = {'addr:city': 'Bielefeld', 'addr:country': 'DE', 'addr:housenumber': '38', 'addr:postcode': '33615', 'addr:street': 'Stapenhorststraße', 'amenity': 'ice_cream', 'food': 'yes', 'indoor_seating': 'yes', 'internet_access': 'no', 'name': 'Eiscafe Venezia', 'note:de': 'im Winter geschlossen', 'opening_hours': 'Mo-Sa 10:00-22:00; Su 11:00-22:00', 'outdoor_seating': 'yes', 'toilets:wheelchair': 'no', 'wheelchair': 'yes', 'wheelchair:description': 'alles ebenerdig, aber Toilette sehr eng ...'}
+
+f_markers = [folium.Marker([ice.lat, ice.lon], popup='Eisdiele', tooltip=r"""<p>{name}<br/>{strasse} {nr}<br/>{stadt}</p>"""
+                           .format(name=ice.tags["name"],strasse=ice.tags["addr:street"],
+                                   nr=ice.tags["addr:housenumber"],stadt=ice.tags["addr:city"])).add_to(my_map) for ice in ice_creams.nodes]
+
+my_map.save("NYC_map.html")
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div([
+    html.H1("Bielefeld City Map"),
+    html.Iframe(id = "map", srcDoc=open("NYC_map.html", "r").read(), width="100%", height="600")
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
