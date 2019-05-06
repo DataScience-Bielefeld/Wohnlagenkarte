9c5d78bf4845589284d2744567aaceeb8d728d82
Nils Hachmeister <nils.hachmeister@mailbox.org>
2019-05-06 17:18:33 +0200

    Added a bokeh based approach.

--- Dash/bokeh_approach.py
+++ Dash/bokeh_approach.py
@@ -1,7 +1,8 @@
 from bokeh.plotting import figure, show, output_notebook
 from bokeh.tile_providers import CARTODBPOSITRON
-
-p = figure(x_range=(-9780000, -9745000), y_range=(5130000, 5160000),
+center = (950000,6803000)
+delta = 18000
+p = figure(x_range=(center[0]-delta,center[0]+delta), y_range=(center[1]-delta,center[1]+delta),
            x_axis_type="mercator", y_axis_type="mercator")
 
 p.add_tile(CARTODBPOSITRON)
